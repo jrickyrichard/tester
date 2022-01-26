8cc33ad433e4973794651c7cc4c9747a7df20703
jrickyrichard <jrrsum1010@gmail.com>
2022-01-27 06:20:30 +0800

    edit app.js

--- app.js
+++ app.js
@@ -4,5 +4,33 @@ function computerPlay(){
     return pick[random];
 }
 
-let res = computerPlay();
-console.log(res)
+function playRound(playerSelection, computerSelection){
+
+    return playerSelection === 'rock' && computerSelection === 'paper'
+    ? 'You lose, paper beats rock'
+    : playerSelection === 'paper' && computerSelection === 'rock'
+    ? 'You win, paper beats rock'
+    : playerSelection === 'scissors' && computerSelection === 'rock'
+    ? 'You lose, rock beats scissors'
+    : playerSelection === 'rock' && computerSelection === 'scissors'
+    ? 'You win, rock beats scissors'
+    : playerSelection === 'paper' && computerSelection === 'scissors'
+    ? 'You lose, scissors beats paper'
+    : playerSelection === 'scissors' && computerSelection === 'paper'
+    ? 'You win, scissors beats paper'
+    : `${playerSelection} vs ${computerSelection}, Stalemate!`
+
+}
+
+function game(){
+
+    for(let i=0; i <= 4; i++){
+        const playerSelection = window.prompt('pick scissors, paper or rock: ');
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game()
+
+
